d5f567d4330af11413278e0f761a680ac4e7d21b
Sushil More <sushilm35@gmail.com>
2021-07-29 09:56:05 +0530

    testimonilas added

--- database/migrations/2021_07_27_083136_create_testimonials_table.php
+++ database/migrations/2021_07_27_083136_create_testimonials_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTestimonialTable extends Migration
+class CreateTestimonialsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTestimonialTable extends Migration
      */
     public function up()
     {
-        Schema::create('testimonial', function (Blueprint $table) {
+        Schema::create('testimonials', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('discription');
